e86becdb4c34f5d60f26efcb822dfca42287fffa
Iryna Alchynska <iren0988@gmail.com>
2025-03-24 22:20:44 +0100

    add: crud and connection

---
+++ app/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = (
+    "postgresql+asyncpg://postgres:mysecretpassword@localhost:5432/contacts_db"
+)
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
---
+++ app/main.py
@@ -0,0 +1,29 @@
+import logging
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from . import routes
+from .models import Base
+from .database import engine
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    logger.info("Starting up the application...")
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    logger.info("Application startup complete.")
+
+    yield  
+
+    logger.info("Shutting down the application...")
+
+
+app = FastAPI(title="Contacts API", lifespan=lifespan)
+app.include_router(routes.router, prefix="/api")
